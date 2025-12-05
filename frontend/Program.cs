8c48401316fd9ea1654b3d1b8a0ca014313814a7
Albin Stenhoff <99albste@gafe.molndal.se>
2025-12-05 19:53:37 +0100

    Fix

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -9,19 +9,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// 1) Vanlig HttpClient för saker som ligger på samma adress som frontenden
+// EN enkel HttpClient som pekar på samma adress som frontenden
 builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-});
-
-// 2) Named HttpClient för din Azure Functions-API (Coingecko + AlphaVantage)
-builder.Services.AddHttpClient("MarketGrowth.Api", client =>
-{
-    // LOKALT: din Functions-adress
-    client.BaseAddress = new Uri("http://localhost:7247/");
-    // När du sedan deployar kan du byta till t.ex:
-    // client.BaseAddress = new Uri("https://DIN-FUNCTION-APP.azurewebsites.net/");
 });
 
 builder.Services.AddMsalAuthentication(options =>
